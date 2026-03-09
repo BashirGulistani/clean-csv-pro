752f57584e6e103c3483c089b3bd86c0cba57cce
Bashir Gulistani <76609477+BashirGulistani@users.noreply.github.com>
2026-03-08 21:55:20 -0400

    Update github_annotations.py

--- github_annotations.py
+++ github_annotations.py
@@ -12,5 +12,22 @@ _SEV_TO_CMD = {"low": "notice", "medium": "warning", "high": "error"}
 
 
 
+def _escape(s: str) -> str:
+
+    if s is None:
+        return ""
+    return (
+        str(s)
+        .replace("%", "%25")
+        .replace("\r", "%0D")
+        .replace("\n", "%0A")
+        .replace(":", "%3A")
+        .replace(",", "%2C")
+    )
+
+
+
+
+
 
 
