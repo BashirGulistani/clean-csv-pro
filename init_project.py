d8f1cfb7dbd45cdfc0ee2fea0b8c5071c5c0c61b
Bashir Gulistani <76609477+BashirGulistani@users.noreply.github.com>
2026-03-19 22:52:06 -0400

    Create init_project.py

---
+++ init_project.py
@@ -0,0 +1,16 @@
+from __future__ import annotations
+
+from dataclasses import dataclass
+from pathlib import Path
+from typing import Iterable, List, Optional
+
+from .baseline import save_baseline
+from .config import make_default_config_json
+
+
+
+
+
+
+
+
