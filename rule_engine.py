10416cbf5632dc415cb243081fccaa917bb8d685
Bashir Gulistani <76609477+BashirGulistani@users.noreply.github.com>
2026-03-31 23:08:26 -0400

    Update rule_engine.py

--- rule_engine.py
+++ rule_engine.py
@@ -15,3 +15,27 @@ except Exception:
 
 
 
+class RuleEngine:
+    """
+    Central rule engine that allows:
+    - core rules
+    - optional rule packs (Shopify, custom, etc.)
+    - dynamic enabling/disabling
+    """
+
+    def __init__(
+        self,
+        include_shopify: bool = True,
+        extra_rules: List[Rule] | None = None,
+    ):
+        self.rules: List[Rule] = list(RULES)
+
+        if include_shopify:
+            self.rules.extend(SHOPIFY_RULES)
+
+        if extra_rules:
+            self.rules.extend(extra_rules)
+
+
+
+
